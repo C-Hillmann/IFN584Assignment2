4f8297abe734269e0e4a4778bdd7940b93a57d2c
Prashant Rizal <prsnt.rizal@gmail.com>
2025-05-20 19:58:55 +1000

    Creating structure for Notakto

---
+++ BaseGame/CompositeNotaktoBoard.cs
@@ -0,0 +1,62 @@
+﻿using BaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseGame
+{
+    public class CompositeNotaktoBoard : IBoard
+    {
+        public NotaktoGame Game { get; set; }
+
+        private List<IBoard> notaktoBoards = new List<>();
+
+        public CompositeNotaktoBoard() {
+            for (int i = 0; i <= 2; i++) {
+                notaktoBoards.Add(new NotaktoBoard()); // adding three individual notakto boards
+            }
+        }
+
+
+        public int Size => 3;
+
+        public string[,] boardGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBoard CloneBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i <= 2; i++) {
+                Console.WriteLine($"Board {i + 1} : ")
+                notaktoBoards[i].Display() + "\n";
+            }
+        }
+
+        public string GetCell(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCell(int row, int col, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IBoard> GetNotaktoBoards() {
+            return notaktoBoards;
+        }
+
+        public IBoard GetNotaktoBoard(int index) {
+            return notaktoBoards[index];
+        }
+    }
+}
---
+++ BaseGame/NotaktoBoard.cs
@@ -0,0 +1,44 @@
+﻿using BaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseGame
+{
+    public class NotaktoBoard : IBoard
+    {
+        public NotaktoGame Game { get; set; }
+
+        private string[,] grid = new string[3,3];
+
+        public int Size => 3;
+
+        public string[,] boardGrid()
+        {
+            return grid;
+        }
+
+        public IBoard CloneBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Display()
+        {
+            //TODO : Display function
+        }
+
+        public string GetCell(int row, int col)
+        {
+            return grid[row, col];
+        }
+
+        public void SetCell(int row, int col, string value)
+        {
+            grid[row, col] = value;
+        }
+    }
+}
